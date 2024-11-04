0221e73a1858ec663d09df8e67592941a1196231
Anders Leino <aleino@nvidia.com>
2024-11-04 14:48:13 +0200

    Make structured buffers illegal parameter types in WGSL
    Structured buffer types translate to array types in the WGSL emitter.
    WGSL doesn't allow passing runtime-sized arrays to functions.
    
    This closes issue #5228.

--- source/slang/slang-ir-specialize-resources.cpp
+++ source/slang/slang-ir-specialize-resources.cpp
@@ -67,7 +67,10 @@ struct ResourceParameterSpecializationCondition : FunctionCallSpecializeConditio
             else
                 return isIllegalGLSLParameterType(type);
         }
-
+        else if (isWGPUTarget(targetRequest))
+        {
+            return isIllegalWGSLParameterType(type);
+        }
 
         // For now, we will not treat any other parameters as
         // needing specialization, even if they use resource
@@ -1349,4 +1352,10 @@ bool isIllegalSPIRVParameterType(IRType* type, bool isArray)
     }
     return false;
 }
+
+bool isIllegalWGSLParameterType(IRType* type)
+{
+    return isIllegalGLSLParameterType(type);
+}
+
 } // namespace Slang
